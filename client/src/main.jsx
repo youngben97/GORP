2e85c16e9e817dc234b8391457c8b6427524adc9
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-19 16:22:38 -0700

    imported react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
-import './index.css'
+import Home from './pages/Home';
+
+//is this the right entry point? ask TA
 //import fonts for Joy UI
 import '@fontsource/inter';
 
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement will changre to ErrorPage component 
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
