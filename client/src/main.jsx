b38a7e27673a413a8f30a9e84dfc9af782573f22
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-19 17:26:24 -0700

    changed dependency to Material UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,11 @@ import App from './App.jsx'
 import Home from './pages/Home';
 
 //is this the right entry point? ask TA
-//import fonts for Joy UI
-import '@fontsource/inter';
+//import roboto for Material UI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const router = createBrowserRouter([
   {
