d442f5e67c7c8e398c0edcc4ef3b5f31f41345c1
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-20 19:38:12 -0700

    mix-item rough draft

--- client/src/components/MixCard.jsx
+++ client/src/components/MixCard.jsx
@@ -1,35 +1,68 @@
 import * as React from 'react';
-import { Paper, Grid, Typography, Box} from '@mui/material';
+import { Box, Grid, ThemeProvider, Typography, createTheme, Stack, Button } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        background: {
+            secondary: '#D6C9C9',
+            tertiary: '#C7D3DD',
+            quaternary: '#6C91BF',
+            dark: '#37393A',
+        },
+        text: {
+            light: '#D9D0DE',
+            dark: '#37393A',
+            link: '#77B6EA',
+        }
+    }
+})
 
 export default function MixCard() {
     return (
-        <Grid item xs={4}>
-            <Paper elevation={2}>
-                <Box paddingX={1}>
-                  <Typography variant = 'h6' gutterBottom sx={{textAlign: 'center'}}>
-                      Trail Mix Name
-                  </Typography>
-                  <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }}
-                  >
-                      <Typography variant='subtitle1' gutterBottom>
-                         Created By: Username
-                      </Typography>
-                      <Typography variant='subtitle2' gutterBottom>
-                         Calorie Count: ###
-                      </Typography>
-                      <Typography variant='subtitle2' gutterBottom>
-                         Protein: ##g
-                      </Typography>
-                      <Typography variant='subtitle2' gutterBottom>GORPs: ##</Typography>
-                  </Box>
-                </Box>
-            </Paper>
-        </Grid>
+        <ThemeProvider theme={theme}>
+          <Grid item xs={6}>
+            <Box
+            sx={{
+                bgcolor: 'background.secondary',
+                boxShadow: 1,
+                borderRadius: 2,
+                minWidth: 300,
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+            >
+                {/* replace text with a link */}
+              <Stack direction='row' sx={{
+                    bgcolor: 'background.dark',
+                    color: 'text.link',
+                    width: 1,
+                    borderRadius: '8px 8px 0 0',
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                    }}>
+                    <Typography variant='button' sx={{ m: 1, fontSize: 'h6.fontSize'}}> Mix Name</Typography>
+                    <Typography sx={{ m:1, color: 'text.light'}}>Username</Typography>
+              </Stack>
+              <Stack direction='row' sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <Stack sx={{ display: 'flex', justifyContent: 'space-between'}}>
+                  <Stack direction='row' alignItems='center' sx={{color: 'text.dark', borderRadius:2, p:1 }}>
+                    <Typography variant='subtitle1'>Calories per serving:</Typography>
+                    <Typography sx={{textAlign: 'center', mx: 1}}> ### </Typography>
+                  </Stack>
+                  <Stack direction='row' alignItems='center' sx={{color: 'text.dark', borderRadius:2, p:1 }}>
+                    <Typography variant='subtitle1'>Protein per serving:</Typography>
+                    <Typography sx={{textAlign: 'center', mx: 1}}> ##g </Typography>
+                  </Stack>
+                </Stack>
+                <Stack direction='row' alignItems='center' sx={{color: 'text.light', display:'flex', justifyContent:'end'}}>
+                  <Button variant="contained" sx={{ m: 1, p:1 }}>GORP</Button>
+                  <Typography sx={{bgcolor: 'background.quaternary', m: 1, borderRadius:2}}>##</Typography>
+                {/* replace gorp with a button */}
+                </Stack>
+              </Stack>
+            </Box>
+          </Grid>
+        </ThemeProvider>
     )
 }
 
