c6c3966d360341d03eaa2033ba11df82aae70898
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-21 15:59:48 -0700

    calling login to look at it

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import SignUp from '../components/SignupForm';
+import Login from '../components/LoginForm';
 
 export default function Home() {
     return (
+        <>
         <SignUp />
+        <Login />
+        </>
     )
 }
 
