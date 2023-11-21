32b96875bbf274f0d23479d7ca7157986d841570
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-20 21:21:42 -0700

    import index

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client'
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import './index.css';
+
 import App from './App.jsx'
 import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage.jsx';
