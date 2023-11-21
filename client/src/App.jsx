80fdd5bfffc123137d0bb35f051beac361ba2631
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-21 15:59:15 -0700

    centralized theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,33 @@ import { Outlet } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header';
 
+
 const theme = createTheme({
   palette: {
-    background: { primary: '#E8EEF2'}
-  }
+    mode: 'light',
+    primary: {
+      main: '#37853a',
+    },
+    secondary: {
+      main: '#f50057',
+      light: '#f53377',
+    },
+    background: {
+      default: '#fff3e0',
+      paper: '#fbf6f6',
+    },
+    error: {
+      main: '#d50000',
+    },
+    divider: '#f50057',
+    text: {
+      primary: 'rgba(0,0,0,0.87)',
+      secondary: '#fbebee',
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
 })
 
 
