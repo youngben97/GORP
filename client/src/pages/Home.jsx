abc248093b971ea8387cec9f8202fdc9dca9d714
LucasF <che.fernandez255@gmail.com>
2023-11-21 19:42:45 -0700

    created grid items for future user mixes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
-
-//consolidate create mix and my mix into this page
-
+import SignUp from '../components/SignupForm';
+import Login from '../components/LoginForm';
+import { Grid, Typography, Box,} from '@mui/material';
 
 export default function Home() {
     return (
-        <>
-        Home
-        </>
+        <Box sx={{
+            flexGrow: 1,
+            minHeight: '400px'
+        }}>
+            <Grid Container>
+                {sectionItems.map((item) => (
+                    <Grid
+                        item
+                        xs={12}
+                        md={3.5}
+                        minHeight={300}
+                        key={item.id}
+                    >
+                        <Typography>{item.sentence}</Typography>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     )
 }
 
