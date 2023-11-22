280f6f46b435deec594d9ad63b38e465e4e73324
LucasF <che.fernandez255@gmail.com>
2023-11-21 19:43:42 -0700

    updated layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,28 +1,31 @@
 import * as React from 'react';
 import SignUp from '../components/SignupForm';
 import Login from '../components/LoginForm';
-import { Grid, Typography, Box,} from '@mui/material';
+import { Grid, Typography, Box, Container,} from '@mui/material';
 
 export default function Home() {
     return (
-        <Box sx={{
+        <Container sx={{
             flexGrow: 1,
             minHeight: '400px'
         }}>
-            <Grid Container>
-                {sectionItems.map((item) => (
-                    <Grid
-                        item
-                        xs={12}
-                        md={3.5}
-                        minHeight={300}
-                        key={item.id}
-                    >
-                        <Typography>{item.sentence}</Typography>
+            <Grid container spacing={16}>
+                    <Grid item xs={12} md={6} minHeight={300}>
+                        <Typography sx={{
+                            fontSize: '3.5rem'
+                        }}>
+                            What the heck is even Gorp anyhow?
+                        </Typography>
                     </Grid>
-                ))}
+                    <Grid item xs={12} md={6} minHeight={300}>
+                        <Typography sx={{
+                            marginTop: '20px'
+                        }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolores eum culpa dolorem a consectetur, aspernatur maiores nemo explicabo aperiam sequi nesciunt assumenda commodi vitae similique vero iure nobis deleniti!
+                        </Typography>
+                    </Grid>
             </Grid>
-        </Box>
+        </Container>
     )
 }
 
