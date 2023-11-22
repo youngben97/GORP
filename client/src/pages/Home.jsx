98ae0dc781589d360989609dd0f7ff723b98379f
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-21 17:04:13 -0700

    removed signup and login from home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import SignUp from '../components/SignupForm';
-import Login from '../components/LoginForm';
+
 
 export default function Home() {
     return (
         <>
-        <SignUp />
-        <Login />
+        Home
         </>
     )
 }
