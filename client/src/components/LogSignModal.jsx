d04be65393e4e7591853f6d16ca68418603a9068
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-21 17:03:59 -0700

    added tab functionality. needs more styling

--- client/src/components/LogSignModal.jsx
+++ client/src/components/LogSignModal.jsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import PropTypes from 'prop-types';
 import SignUpForm from './SignupForm';
 import LoginForm from './LoginForm';
+import { AppBar, Box, Button, Typography, Modal, Tab, Tabs } from '@mui/material';
 
 const style = {
     position: 'absolute',
@@ -18,11 +16,53 @@ const style = {
     p: 4,
   };
 
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+function a11yProps(index) {
+    return {
+      id: `full-width-tab-${index}`,
+      'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
   export default function LogSignModal() {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const [value, setValue] = React.useState(0);
 
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setValue(index);
+      };
+    
     return (
         <>
             <Button onClick={handleOpen} sx={{color: 'background.paper'}}>Login/Sign up</Button>
@@ -32,14 +72,20 @@ const style = {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-               <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                </Box>
+              <Box sx={style}>  
+                <AppBar position="static">
+                  <Tabs value={value} onChange={handleChange} indicatorColor='secondary' textColor='inherit' variant='fullWidth' aria-label="login/signup tabs">
+                    <Tab label="Item One" {...a11yProps(0)} />
+                    <Tab label="Item Two" {...a11yProps(1)} />
+                  </Tabs>
+                </AppBar>
+                <CustomTabPanel value={value} index={0}>
+                  <LoginForm />
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={1}>
+                  <SignUpForm />
+                </CustomTabPanel>
+              </Box>
             </Modal>
         </>
     )
