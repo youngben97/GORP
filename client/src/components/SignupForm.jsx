099beef946afe207f971286a5e1be6a4a81a39a6
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-25 18:14:41 -0700

    incorporated CREATE_USER mutation into component

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,4 +1,11 @@
 import * as React from 'react';
+import { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+import { CREATE_USER } from '../utils/mutation';
+
+import Auth from '../utils/auth';
+
 import {
     Avatar,
     Button,
@@ -11,19 +18,42 @@ import {
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-export default function SignUp() {
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      console.log({
-        email: data.get('email'),
-        password: data.get('password'),
-      });
-    };
+const SignUp = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [createUser, { error, data }] = useMutation(CREATE_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+        const { data } = await createUser({
+            variables: { ...formState},
+        });
+
+        Auth.login(data.createUser.token);
+    } catch (e) {
+        console.error(e);
+    }
+  };
 
 return (
-    <Container component='main' maxWidth='xs'>
+    // <Container component='main' maxWidth='xs'>
         <Box
+          maxWidth='xs'
           sx={{
             marginTop: 8,
             display: 'flex',
@@ -37,7 +67,7 @@ return (
             <Typography component='h1' variant='h5'>
                 Sign up
             </Typography>
-            <Box component = 'form' noValidate onSubmit={handleSubmit} sx={{ mt: 3}}>
+            <Box component = 'form' noValidate onSubmit={handleFormSubmit} sx={{ mt: 3}}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <TextField
@@ -48,6 +78,7 @@ return (
                             id="username"
                             label="Username"
                             autoFocus
+                            onChange={handleChange}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -58,6 +89,7 @@ return (
                         label="Email Address"
                         name="email"
                         autoComplete="email"
+                        onChange={handleChange}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -69,6 +101,7 @@ return (
                         type="password"
                         id="password"
                         autoComplete="new-password"
+                        onChange={handleChange}
                     />
                     </Grid>
                 </Grid>
@@ -82,172 +115,8 @@ return (
                 </Button>
             </Box>
         </Box>
-    </Container>
+    // </Container>
     )
-}
+};
 
-// const SignUpForm = () => {
-//     const [firstName, setFirstName] = useState('')
-//     const [lastName, setLastName] = useState('')
-//     const [email, setEmail] = useState('')
-//     const [password, setPassword] = useState('')
- 
-//     function handleSubmit(event) {
-//         event.preventDefault();
-//         console.log(firstName, lastName, email, password) 
-//     }
- 
-//     return (
-//         <React.Fragment>
-//             <h2>Say pard', are you ready to git on yer GORP adventure? Sign up to GORP OUT!</h2>
-//             <form onSubmit={handleSubmit} action={<Link to="/login" />}>
-//                 <Stack spacing={2} direction="row" sx={{marginBottom: 4}}>
-//                     <TextField
-//                         type="text"
-//                         variant='outlined'
-//                         color='secondary'
-//                         label="First Name"
-//                         onChange={e => setFirstName(e.target.value)}
-//                         value={firstName}
-//                         fullWidth
-//                         required
-//                         sx={{ 
-//                             display: 'flex', 
-//                             justifyContent: 'center' 
-//                         }}
-//                     />
-//                     <TextField
-//                         type="text"
-//                         variant='outlined'
-//                         color='secondary'
-//                         label="Last Name"
-//                         onChange={e => setLastName(e.target.value)}
-//                         value={lastName}
-//                         fullWidth
-//                         required
-//                     />
-//                 </Stack>
-//                 <TextField
-//                     type="email"
-//                     variant='outlined'
-//                     color='secondary'
-//                     label="Email"
-//                     onChange={e => setEmail(e.target.value)}
-//                     value={email}
-//                     fullWidth
-//                     required
-//                     sx={{mb: 4}}
-//                 />
-//                 <TextField
-//                     type="password"
-//                     variant='outlined'
-//                     color='secondary'
-//                     label="Password"
-//                     onChange={e => setPassword(e.target.value)}
-//                     value={password}
-//                     required
-//                     fullWidth
-//                     sx={{mb: 4}}
-//                 />
-//                 <Button variant="outlined" color="secondary" type="submit">Register</Button>
-//             </form>
-//             <small>Already have an account? <Link to="/login">Login Here</Link></small>
-     
-//         </React.Fragment>
-//     )
-// }
- 
-// export default SignUpForm;
-
-// import { Button, TextField, Typography, createTheme } from "@mui/material";
-// import { red } from "@mui/material/colors";
-// import React, { useState } from "react";
-
-// const theme = createTheme({
-//     palette: {
-//         backg: ''
-//     }
-// })
-
-// export default function SignUp () {
-//     const [userFormData, setUserFormData] = useState({username: '', email: '', password: ''});
-
-//     const handleInputChange = (event) => {
-//         const {name, value} = event.target;
-//         setUserFormData({...userFormData, [name]: value})
-//     }
-//         // add logic for handling form submission once addUser functionality is written
-//     // const handleFormSubmit =
-
-//     return (
-//         <React.Fragment>
-//             <Typography sx={{
-//                 display: 'flex',
-//                 justifyContent: 'center',
-//                 height: '10vh',
-//                 border: 1,
-//                 borderRadius: '16px',
-//                 gap: '16px',
-//                 flexDirection: 'column'
-//             }}>
-//                 Ready to GORP out? Sign up to start making your concoctions today!
-//             </Typography>
-//                 <form>
-//                     <TextField
-//                         label="email"
-//                         type="email"
-//                         placeholder="Your email"
-//                         name="email"
-//                         sx={{
-//                             display: 'flex',
-//                             justifyContent: 'center',
-//                             height: '10vh',
-//                             gap: '16px',
-//                             flexDirection: 'column',
-//                             '& .MuiOutlinedInput-root fieldset': {
-//                                 border: '5px solid',
-//                                 borderRadius: '16px',
-//                                 borderColor: 'pink'
-//                         }}}
-//                     />
-//                 </form>
-//                 <form>
-//                     <TextField
-//                         label="username"
-//                         type="text"
-//                         placeholder='Your username'
-//                         name='username' 
-//                         sx={{
-//                             display: 'flex',
-//                             justifyContent: 'center',
-//                             height: '10vh',
-//                             gap: '16px',
-//                             flexDirection: 'column'
-//                         }}
-//                     />
-//                 </form>
-//                 <form>
-//                     <TextField
-//                         label="password"
-//                         type="password"
-//                         placeholder="Your password"
-//                         name="password"
-//                         required
-//                         sx={{
-//                             display: 'flex',
-//                             justifyContent: 'center',
-//                             height: '10vh',
-//                             gap: '16px',
-//                             flexDirection: 'column'
-//                         }}
-//                     />
-//                 </form>
-//                 <Button
-//                     disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-//                     type="submit"
-//                     variant="success">
-//                         Submit
-//                 </Button>
-//         </React.Fragment>
-//     )
-// }   
+export default SignUp;
