509cbaf8675c29e7a554aa735b293f19ac802c59
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-25 18:16:49 -0700

    added logout/rendering for buttons. not done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,24 +2,42 @@ import * as React from 'react';
 import { AppBar, Box, Toolbar, Button, Stack, Modal} from '@mui/material';
 import LogSignModal from './LogSignModal';
 
-//Will need to incorporate Auth
-// import Auth from '../utils/auth';
-
+import Auth from '../utils/auth';
 export default function Navbar() {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
+    //need to add title element back in and style buttons
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position='static'>
                 <Toolbar sx={{justifyContent: 'space-between'}}>
-                        <Button href='/' color='inherit' sx={{ typography: 'h1'}}>GORP</Button>
-                        <Stack direction='row'>
-                            <Button href='/browsemix' color='inherit' sx={{mx: 2}}>Mix it Up!</Button>
-                            <LogSignModal/>
-                        </Stack>
+                    {Auth.loggedIn() ? (
+                        <>
+                        <Button href='/' color='inherit' sx={{mx: 2}}>
+                            {Auth.getProfile().data.username}'s mixes
+                        </Button>
+                        <Button href='/browsemix' color='inherit' sx={{mx: 2}}>Browse Mixes</Button>
+                        <Button color='inherit' onClick={logout}>
+                            Logout
+                        </Button>
+                        </>
+                    ) : (
+                        <LogSignModal />
+                    )}
                 </Toolbar>
             </AppBar>
         </Box>
     )
 }
+
+{/* <Stack direction='row'>
+<Button href='/browsemix' color='inherit' sx={{mx: 2}}>Mix it Up!</Button>
+<LogSignModal/>
+</Stack> */}
+
 
                             {/* <Link onClick={Auth.logout}>Logout</Link>         */}
 
