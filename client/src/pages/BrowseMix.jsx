16e8d00e84791e7d9ec94fb7ba84144dad3cd2d6
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-25 21:22:25 -0700

    columns for app layout. components go in boxes

--- client/src/pages/BrowseMix.jsx
+++ client/src/pages/BrowseMix.jsx
@@ -1,89 +1,25 @@
 import * as React from 'react';
 // import { styled } from '@mui/material/styles';
 // import Box from '@mui/material/Box';
-import MixCard from '../components/MixCard';
-import { Container, Grid } from '@mui/material';
-
-// {/* <Grid container spacing={2}>
-//   <Grid item xs={8}>
-//     <Item>xs=8</Item>
-//   </Grid>
-//   <Grid item xs={4}>
-//     <Item>xs=4</Item>
-//   </Grid>
-//   <Grid item xs={4}>
-//     <Item>xs=4</Item>
-//   </Grid>
-//   <Grid item xs={8}>
-//     <Item>xs=8</Item>
-//   </Grid>
-// </Grid> */}
+// import MixCard from '../components/MixCard';
+import { Container, Grid, Box } from '@mui/material';
 
 export default function BrowseMix() {
     return (
-        <Container>
-            <Grid container spacing={5}>
-                <MixCard />
-                <MixCard />
-                <MixCard />
-                <MixCard />
-                <MixCard />
-                <MixCard />
+        <Container sx={{ height: '100vh' }}>
+            <Grid container spacing={2} sx={{ height: '100%', justifyContent: 'space-evenly'  }}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                </Grid>
             </Grid>
         </Container>
     )
 }
 
 //use code for iterating user mixes into grid items
-
-// import React from 'react';
-// import { Grid, Typography, Box } from '@mui/material';
-// import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
-// import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
-// import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
-// import useStyles from '../styles/styles';
-
-// const Section = () => {
-//   const classes = useStyles();
-
-//   const sectionItems = [
-//     {
-//       id: 1,
-//       icon: <EngineeringOutlinedIcon sx={{ fontSize: 100 }} color="primary" />,
-//       sentence:
-//         'Solving world problems through various web applications using efficient programs and tools',
-//     },
-//     {
-//       id: 2,
-//       icon: <AllInclusiveIcon sx={{ fontSize: 100 }} color="primary" />,
-//       sentence:
-//         'Through team work, we collaborate and deliver quality projects of high standards',
-//     },
-//     {
-//       id: 3,
-//       icon: <PaidOutlinedIcon sx={{ fontSize: 100 }} color="primary" />,
-//       sentence: 'Flexible payment plan is applicable to all our services',
-//     },
-//   ];
-//   return (
-//     <Box sx={{ flexGrow: 1, minHeight: '400px' }}>
-//       <Grid container className={classes.sectionGridContainer}>
-//         {sectionItems.map((item) => (
-//           <Grid
-//             item
-//             xs={12}
-//             md={3.5}
-//             minHeight={300}
-//             key={item.id}
-//             className={classes.sectionGridItem}
-//           >
-//             {item.icon}
-//             <Typography>{item.sentence}</Typography>
-//           </Grid>
-//         ))}
-//       </Grid>
-//     </Box>
-//   );
-// };
-
-// export default Section;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,29 +1,19 @@
 import * as React from 'react';
-import SignUp from '../components/SignupForm';
-import Login from '../components/LoginForm';
 import { Grid, Typography, Box, Container,} from '@mui/material';
 
 export default function Home() {
     return (
-        <Container sx={{
-            flexGrow: 1,
-            minHeight: '400px'
-        }}>
-            <Grid container spacing={16}>
-                    <Grid item xs={12} md={6} minHeight={300}>
-                        <Typography sx={{
-                            fontSize: '3.5rem'
-                        }}>
-                            What the heck is even Gorp anyhow?
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={6} minHeight={300}>
-                        <Typography sx={{
-                            marginTop: '20px'
-                        }}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolores eum culpa dolorem a consectetur, aspernatur maiores nemo explicabo aperiam sequi nesciunt assumenda commodi vitae similique vero iure nobis deleniti!
-                        </Typography>
-                    </Grid>
+        <Container sx={{ height: '100vh' }}>
+            <Grid container spacing={2} sx={{ height: '100%', justifyContent: 'space-evenly'  }}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                </Grid>
             </Grid>
         </Container>
     )
