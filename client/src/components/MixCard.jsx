9316fcfdbb0616de2b367ea321cf96d439dcca1e
LucasF <che.fernandez255@gmail.com>
2023-11-27 21:29:22 -0700

    wrote card component to hold mix data

---
+++ client/src/components/MixCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardContent, Typography} from '@mui/icons-material';
+
+const MixCard = ({ mix}) => {
+    return (
+        <Card>
+            <CardContent>
+                <Typography variant='h5' gutterBottom>
+                    {mix.mixName}
+                </Typography>
+                <Typography>{mix.mixCreator}</Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default MixCard;
