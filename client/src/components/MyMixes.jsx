18015e698681e46a3efa4fa767d19f54b367756a
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-28 11:50:44 -0700

    incoporated querymix for macro data.

--- client/src/components/MyMixes.jsx
+++ client/src/components/MyMixes.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { Box, Typography, Stack, Button, Modal } from '@mui/material';
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_ME } from '../utils/queries';
-import { REMOVE_MIX } from '../utils/mutation';
+import { Box, Typography, Stack, Button, Modal, TextField } from '@mui/material';
+import { useQuery, useMutation, useApolloClient } from '@apollo/client';
+import { QUERY_ME, QUERY_MIX } from '../utils/queries';
+import { REMOVE_MIX, ADD_COMMENT } from '../utils/mutation';
 
 const style = {
   position: 'absolute',
@@ -20,18 +20,22 @@ const style = {
   
 };
 
-//need to add delete button mutation (in modal?)
+
 //need to replace mixname typography with an actual button
 
 export default function MyMixes() {
+  const client = useApolloClient();
   const { loading, error, data } = useQuery(QUERY_ME);
   const [openModal, setOpenModal] = React.useState([]);
   const [removeMix] = useMutation(REMOVE_MIX);
+  const [addComment] = useMutation(ADD_COMMENT);
+  const [mixData, setMixData] = React.useState(null); // State to store mix query data
   
   const handleOpen = (index) => {
     const newOpenModal = [...openModal];
     newOpenModal[index] = true;
     setOpenModal(newOpenModal);
+    handleQueryMix(data.me.mixes[index]._id);
   };
   const handleClose = (index) => {
     const newOpenModal = [...openModal];
@@ -49,7 +53,23 @@ export default function MyMixes() {
     } catch (error) {
       console.error('Error deleting mix:', error);
     }
-  }
+  };
+
+  const handleAddComment = async(mixId, index) => {}
+
+  const handleQueryMix = async (mixId) => {
+    try {
+      // Trigger the mix query here
+      const { data: mixData } = await client.query({
+        query: QUERY_MIX,
+        variables: { mixId }
+      });
+      console.log('Mix data:', mixData);
+      setMixData(mixData); // Store mix query data in state
+    } catch (error) {
+      console.error('Error querying mix:', error);
+    }
+  };
 
   if (loading) return <p>Loading...</p>;
   //change error to styled typography
@@ -75,6 +95,26 @@ export default function MyMixes() {
               >
                 <Stack sx={style}>
                   <Typography variant='h4'>{mix.mixName}</Typography>
+                  {mixData && (
+                    <>
+                      <Typography variant='subtitle1'>Total Calories: {mixData.getMix.totalCalories}</Typography>
+                      <Typography variant='subtitle1'>Total Protein: {mixData.getMix.totalProtein}</Typography>
+                      <Typography variant='subtitle1'>Total Fats: {mixData.getMix.totalFats}</Typography>
+                      <Typography variant='subtitle1'>Total Carbs: {mixData.getMix.totalCarbs}</Typography>
+                      <Typography variant='subtitle1'>Total Sodium: {mixData.getMix.totalSodium}</Typography>
+                    </>
+                  )}
+                  <Box
+                    component="form"
+                    sx={{
+                      '& > :not(style)': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField id="comment-box" variant="outlined" />
+                    <Button variant='contained'>Comment</Button>
+                  </Box>
                   <Button variant='contained' onClick={() => handleDeleteMix(mix._id, index)}>
                     Delete Mix
                   </Button>
