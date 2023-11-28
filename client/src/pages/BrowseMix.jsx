a37ff73a0aeba3a6279ad9a3ca0d0576c309df3a
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-27 18:09:05 -0700

    placeholder text for things we need to build

--- client/src/pages/BrowseMix.jsx
+++ client/src/pages/BrowseMix.jsx
@@ -2,20 +2,29 @@ import * as React from 'react';
 // import { styled } from '@mui/material/styles';
 // import Box from '@mui/material/Box';
 // import MixCard from '../components/MixCard';
-import { Container, Grid, Box } from '@mui/material';
+import { Container, Grid, Box, Typography } from '@mui/material';
 
 export default function BrowseMix() {
     return (
         <Container sx={{ height: '100vh' }}>
             <Grid container spacing={2} sx={{ height: '100%', justifyContent: 'space-evenly'  }}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}>
+                    <Typography sx={{ color: 'text.primary'}}>Community mixes here- think of name</Typography>
+                    <Typography sx={{ color: 'text.primary'}}>list item components for each, can be selected</Typography>
+                    </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}>
+                    <Typography sx={{ color: 'text.primary'}}>nutrition facts like on home</Typography>
+                    </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}></Box>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1 }}>
+                    <Typography sx={{ color: 'text.primary'}}>comments here</Typography>
+                    <Typography sx={{ color: 'text.primary'}}>list item component for each comment</Typography>
+                    <Typography sx={{ color: 'text.primary'}}>comment button</Typography>
+                    </Box>
                 </Grid>
             </Grid>
         </Container>
