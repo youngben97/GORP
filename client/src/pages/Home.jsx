3a961261042d72d323dec752b5245e7b23204c7a
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-28 20:37:44 -0700

    styles

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,13 +21,21 @@ export default function Home() {
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1, borderRadius: '16px' }}>
-                    <NutritionData/>
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1, borderRadius: '16px', p:1 }}>
+                        <Stack sx={{ bgcolor:'background.paper', direction: 'column', alignContent: 'center', justifyContent: 'center', m:2, borderRadius: 1 }}>
+                            <Stack sx={{ bgcolor: 'primary.main', direction: 'column', alignContent: 'center', justifyContent: 'center', m:1, borderRadius: 1}}>
+                                <Typography variant= 'h6' sx={{ color: 'text.secondary', m:1, textAlign: 'center' }}>As you build your mix, you will see totals of different macronutrients here.</Typography>
+                                <Typography variant='body1' sx={{ color: 'text.secondary', m:1, textAlign: 'center'}}>Macronutrient totals are based on an oz of an ingredient.</Typography>
+                            </Stack>
+                            <NutritionData/>
+                        </Stack>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1, borderRadius: '16px' }}>
-                    <MyMixes />
+                    <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1, borderRadius: '16px', p:1 }}>
+                      <Stack sx={{ bgcolor:'background.paper', direction: 'column', alignContent: 'center', justifyContent: 'center', m:2, borderRadius: 1 }}>
+                        <MyMixes/>
+                      </Stack>
                     </Box>
                 </Grid>
             </Grid>
