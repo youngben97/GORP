4656443b0ea0b44c2aa0329e03f44180e8809023
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-29 11:17:43 -0700

    imported and incorporated components

--- client/src/pages/BrowseMix.jsx
+++ client/src/pages/BrowseMix.jsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 // import Box from '@mui/material/Box';
 // import MixCard from '../components/MixCard';
 import { Container, Grid, Box, Typography } from '@mui/material';
-import CommunityMixes from '../components/CommunityMixes';
+import CommunityMixes from '../components/BrowseComponent/CommunityMixes';
+import IngredientNutrient from '../components/BrowseComponent/IngrNutr';
 
 export default function BrowseMix() {
     return (
@@ -18,7 +19,7 @@ export default function BrowseMix() {
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     <Box sx={{ bgcolor: 'secondary.main', height: '100%', flexGrow: 1, borderRadius: '16px' }}>
-                    <Typography sx={{ color: 'text.primary'}}>nutrition facts like on home</Typography>
+                    <IngredientNutrient/>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
