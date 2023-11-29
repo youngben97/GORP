11aaa3d9023a7090de57904dc92cf9355f85806b
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-28 18:53:00 -0700

    cleared clutter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,15 +7,8 @@ import './index.css';
 import App from './App.jsx'
 import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage.jsx';
-// import UserMix from './pages/UserMix.jsx';
 import BrowseMix from './pages/BrowseMix.jsx';
 
-//is this the right entry point? ask TA
-//import roboto for Material UI
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
 
 const router = createBrowserRouter([
   {
