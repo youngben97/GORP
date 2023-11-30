b07786c0964a9aedc338c4fef18c15f9aff7cf94
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-29 17:16:52 -0700

    debugged comment not rendering when it's made

--- client/src/components/BrowseComponent/MixComments.jsx
+++ client/src/components/BrowseComponent/MixComments.jsx
@@ -1,62 +1,38 @@
 import React from 'react';
 import { Stack, Typography, Box, TextField, Button } from '@mui/material';
 import { useMixContext } from '../../MixContext';
-import { useMutation, useQuery, useApolloClient } from '@apollo/client';
+import { useMutation, useApolloClient } from '@apollo/client';
 import { QUERY_MIX } from '../../utils/queries';
 import { ADD_COMMENT, REMOVE_COMMENT } from '../../utils/mutation';
 
 export default function MixComments() {
   const client = useApolloClient();
-  const { currentMixDetails, refetchMixDetails } = useMixContext();
+  const { currentMixDetails, updateMixDetails } = useMixContext();
   const [addComment] = useMutation(ADD_COMMENT);
   const [removeComment] = useMutation(REMOVE_COMMENT);
   const [commentText, setCommentText] = React.useState(''); // State for comment text
   const [comments, setComments] = React.useState([]); // State for comments
-
+ 
 
   const handleAddComment = async (mixId, commentText) => {
     try {
-      console.log('Mix ID:', mixId);
-      console.log('Comment Text:', commentText);
+      // console.log('Mix ID:', mixId);
+      // console.log('Comment Text:', commentText);
       await addComment({
         variables: { mixId, commentText }
       });
-  
-      console.log('Comment added successfully');
-      refetchMixDetails(mixId);
-    } catch (error) {
-      console.error('Error adding comment:', error);
-      console.error('GraphQL error details:', error.graphQLErrors);
-      console.error('Network error details:', error.networkError);
-    }
-  };
 
-  const handleRemoveComment = async (mixId, commentId) => {
-    try {
-      await removeComment({
-        variables: {mixId, commentId}
-      });
-      console.log('Comment deleted');
-      handleQueryMix(mixId);
-    } catch (error) {
-      console.error('Error deleting comment:', error.message);
-      console.error('GraphQL error details:', error.graphQLErrors);
-      console.error('Network error details:', error.networkError);
-    }
-  };
-
-  const handleQueryMix = async (mixId) => {
-    try {
-      // Trigger the mix query here
       const { data: mixData } = await client.query({
         query: QUERY_MIX,
-        variables: { mixId }
+        variables: { mixId },
       });
-      console.log('Mix data:', mixData);
-      // Set comments in state
-      setComments(mixData.getMix.comments || []);
+
+
+      updateMixDetails(mixData.getMix);
+      setCommentText('');
+      console.log('Comment added successfully');
     } catch (error) {
-      console.error('Error querying mix:', error);
+      console.error('Error adding comment:', error);
       console.error('GraphQL error details:', error.graphQLErrors);
       console.error('Network error details:', error.networkError);
     }
@@ -81,7 +57,7 @@ export default function MixComments() {
             onSubmit={(e) => {
               e.preventDefault();
               const comment = e.target.elements.comment.value;
-              const mixId = currentMixDetails._id; // Assuming the mixId is available in currentMixDetails
+              const mixId = currentMixDetails._id;
               handleAddComment(mixId, comment);
             }}
             sx={{
