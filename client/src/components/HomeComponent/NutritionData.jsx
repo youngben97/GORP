e7da4baebd351934695586a1c0342864b0b73060
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-29 21:35:31 -0700

    changed to grid. needs more work

--- client/src/components/HomeComponent/NutritionData.jsx
+++ client/src/components/HomeComponent/NutritionData.jsx
@@ -1,25 +1,40 @@
 import * as React from 'react';
-import { Stack, Typography, Box } from '@mui/material';
+import { Stack, Typography, Grid } from '@mui/material';
 import { useMixContext } from '../../MixContext';
 
-
 const NutritionData = () => {
-    const { totals, setTotals } = useMixContext();
-  
-    return (
-      <Stack sx={{ alignItems: 'center', justifyContent: 'center'}}>
-        <Typography sx={{m:1, p:1, color: 'background.default'}}>Calories:</Typography>
-        <Typography sx={{color: 'background.default'}}>{totals.calories}</Typography>
-        <Typography sx={{m:1, p:1, color: 'background.default'}}>Protein:</Typography>
-        <Typography sx={{color: 'background.default'}}>{totals.protein}</Typography>
-        <Typography sx={{m:1, p:1, color: 'background.default'}}>Fats:</Typography>
-        <Typography sx={{color: 'background.default'}}>{totals.fats}</Typography>
-        <Typography sx={{m:1, p:1, color: 'background.default'}}>Carbs:</Typography>
-        <Typography sx={{color: 'background.default'}}>{totals.carbs}</Typography>
-        <Typography sx={{m:1, p:1, color: 'background.default'}}>Sodium:</Typography>
-        <Typography sx={{mb: 2, color: 'background.default' }}>{totals.sodium}</Typography>
-      </Stack>
-    );
-  };
-  
-  export default NutritionData;
+  const { totals } = useMixContext();
+
+  return (
+    <Grid container rowSpacing={2} sx={{ justifyContent: 'center', alignItems: 'center' }}>
+      <Grid item xs={6}>
+        <Stack sx={{ bgcolor: 'primary.main', m: 1, p: 1, flex: 1 }}>
+          <Typography sx={{ color: 'background.default', textAlign: 'center' }}>Calories:</Typography>
+          <Typography sx={{ bgcolor: 'background.default', textAlign: 'center' }}>{totals.calories}</Typography>
+        </Stack>
+        <Stack sx={{ bgcolor: 'primary.main', m: 1, p: 1, flex: 1 }}>
+          <Typography sx={{ color: 'background.default', textAlign: 'center' }}>Protein:</Typography>
+          <Typography sx={{ bgcolor: 'background.default', textAlign: 'center' }}>{totals.protein}</Typography>
+        </Stack>
+      </Grid>
+      <Grid item xs={6}>
+        <Stack sx={{ bgcolor: 'primary.main', m: 1, p: 1, flex: 1 }}>
+          <Typography sx={{ color: 'background.default', textAlign: 'center' }}>Fats:</Typography>
+          <Typography sx={{ bgcolor: 'background.default', textAlign: 'center' }}>{totals.fats}</Typography>
+        </Stack>
+        <Stack sx={{ bgcolor: 'primary.main', m: 1, p: 1, flex: 1 }}>
+          <Typography sx={{ color: 'background.default', textAlign: 'center' }}>Carbs:</Typography>
+          <Typography sx={{ bgcolor: 'background.default', textAlign: 'center' }}>{totals.carbs}</Typography>
+        </Stack>
+      </Grid>
+      <Grid item xs={6}>
+        <Stack sx={{ bgcolor: 'primary.main', m: 1, p: 1, flex: 1 }}>
+          <Typography sx={{ color: 'background.default', textAlign: 'center' }}>Sodium:</Typography>
+          <Typography sx={{ bgcolor: 'background.default', textAlign: 'center' }}>{totals.sodium}</Typography>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default NutritionData;
