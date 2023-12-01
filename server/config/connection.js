4ee1a8152e015a3f7d0371c802847a3facb31ea9
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-30 18:40:33 -0700

    renamed variable for railway

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gorp');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/gorp');
 
 module.exports = mongoose.connection;
